a24a6256df2750007a215ef6b53c50116b143ff7
Christoffer Trier Månsson <ctm@clin.au.dk>
2025-01-20 11:38:51 +0100

    first document() of package

---
+++ R/add_run_to_list.R
@@ -0,0 +1,72 @@
+#' Add a new AVENIO run to patient overview
+#' 
+#' This function takes the main list of NGS data and adds the information from
+#' a new run to list. If the patient ID/CPR is not present in the main list
+#' the patient is added to the list. If a patient ID/CPR is present in the list 
+#' the new NGS data is added under that patient and all the BAM files for that
+#' patient is manually checked for mutations. 
+#' @importFrom dplyr select `%>%`
+#' @param master_list The `list` of `data.frames` which contains all information on
+#'  on the patients. This is read using 
+#'  readRDS("//Synology_m1/Synology_folder/AVENIO/AVENIO_results_patients.rds")
+#' @param  Directory `Character` string with the full path to the directory on
+#'  the Synology where the data is located.
+#' @return A `list` of `data.frames` with similar structure to `master_list`. 
+#'  During the analysis the number of patients and NGS runs present in the 
+#'  dataset before and after the NGS run is printed. If the list successfully 
+#'  has been updated the `.rds` file on the Synology containing the list is 
+#'  automatically updated. 
+#' @examples
+#'  master <- readRDS("//Synology_m1/Synology_folder/AVENIO/AVENIO_results_patients.rds")
+#'  test_path <- "//Synology_m1/Synology_folder/AVENIO/AVENIO_results/Plasma-ANei_Hsi__lGmaJ1k_Ji_d3O"
+#'  add_run_to_list(master_list = master,
+#'                  Directory = test_path)
+#'  @export
+add_run_to_list <- function(master_list, Directory){
+    if (!is.list(master_list)) {
+        stop("df_list has to be a list")
+    }
+    for(i in 1:length(master_list)){
+        if(!is.data.frame(master_list[[i]])){
+            stop(paste0("Entry ",i," in the master_list is not a data.frame"))
+        }
+    }
+    if (!isScalarCharacter(Directory)) {
+        stop("Directory has to be a character")
+    }
+    if (!grepl("//Synology",Directory)) {
+        stop("'//Synology' has to be the first characters in the Directory string")
+    }
+    if (!file.exists(Directory)) {
+        stop("The path entered as Directory does not exist")
+    }
+    samples <- add_samples(Directory)
+    n_patients_before <- length(master_list)
+    unlisted_before <- do.call(rbind,master_list) %>% 
+        dplyr::select(Analysis.Name,Sample.ID) %>% 
+        unique()
+    n_runs_before <- nrow(unlisted_before)
+    df_list <- create_df_list(samples)
+    reanalyzed <- reanalyze_samples(master_list,df_list)
+    if(is.list(reanalyzed)){
+        if(length(reanalyzed) >= length(master_list)){
+            n_patients_after <- length(reanalyzed)
+            unlisted_after <- do.call(rbind,reanalyzed) %>% 
+                dplyr::select(Analysis.Name,Sample.ID) %>% 
+                unique()
+            n_runs_after <- nrow(unlisted_after)
+            print("Before the dataset consisted of ",
+                  n_patients_before, 
+                  " patients and ",
+                  n_runs_before, 
+                  " samples analyzed")
+            print("Now the dataset consists of ",
+                  n_patients_after, 
+                  " patients and ",
+                  n_runs_after, 
+                  " samples analyzed")
+            saveRDS(reanalyzed,file = "//Synology_m1/Synology_folder/AVENIO/AVENIO_results_patients.rds")
+        }
+    }
+    return(reanalyzed)
+}
---
+++ R/create_simple_output.R
@@ -0,0 +1,87 @@
+#' Get a simple overview for a patient
+#' 
+#' This function takes the main list of NGS data for each patient
+#' located at "//Synology_m1/Synology_folder/AVENIO/AVENIO_results_patients.rds"
+#' and a CPR number (or other unique patient number used for the patient) and 
+#' creates a simple overview of the mutations identified.
+#' @importFrom dplyr left_join select mutate tibble `%>%` filter
+#' @importFrom readxl read_xlsx
+#' @param df_list The `list` of `data.frames` which contains all information on
+#'  on the patients. This is read using 
+#'  readRDS("//Synology_m1/Synology_folder/AVENIO/AVENIO_results_patients.rds")
+#' @param  CPR `Character` string of the unique CPR number or other unique 
+#'  number for the patient of interest. Available CPR numbers can be viewed
+#'  using names(`df_list`)
+#' @param synonymous `Boolean` determining whether synonymous mutations are kept
+#'  in the output. If `TRUE` (default) synonymous mutations are kept in the 
+#'  output. If `FALSE` the synonymous mutations are excluded. 
+#' @return A `list` of length = 2. The first entry ("Patient CPR:") shows the 
+#'  patient of interest CPR/ID number. The second entry ("Results") is a `tibble`
+#'  with 14 variables. Each row corresponds to a mutation identified in any 
+#'  sample. Important variables include sample_index which shows the project the
+#'  patient is connected to, the name of the patient in that project and the 
+#'  date of of the sample collection. Sample.ID is the name of the sample in the
+#'  AVENIO system. Sample_note is the note specifying what type of blood 
+#'  sample the sample is in relation to treatment (BL, Tx, BC, or Unknown). 
+#'  Material specifies what type of material has been analyzed (cfDNA, BC, 
+#'  cfChIP, size-selection, etc.) The Flags variable shows if the mutation only
+#'  has been found in the BAM file and not in the AVENIO output, and/or if the
+#'  mutation also has been found in a BC sample if such sample has been run for
+#'  the patient. If a sample has been analyzed but no mutation was identified 
+#'  `NA` is in all relevant variables. 
+#'  @examples
+#'  master <- readRDS("//Synology_m1/Synology_folder/AVENIO/AVENIO_results_patients.rds")
+#'  names(master)
+#'  create_simple_output(df_list = master,
+#'                       CPR = "1234567890",
+#'                       synonymous = FALSE)
+#'  @export
+create_simple_output <- function(df_list,CPR,synonymous = TRUE){
+    if (!is.list(df_list)) {
+        stop("df_list has to be a list")
+    }
+    for(i in 1:length(df_list)){
+       if(!is.data.frame(df_list[[i]])){
+           stop(paste0("Entry ",i," in the df_list is not a data.frame"))
+       }
+    }
+    if (!isScalarLogical(synonymous)) {
+        stop("synonymous has to be a TRUE or FALSE")
+    }
+    if (!isScalarCharacter(CPR)) {
+        stop("CPR has to be a character")
+    }
+    df <- df_list[[CPR]]
+    if(is.null(df)){
+        stop("CPR number is not in the dataset")
+    }
+    print("Reading AVENIO_runs.xlsx file")
+    AVENIO_runs <- readxl::read_xlsx("//Synology_m1/Synology_folder/AVENIO/AVENIO_runs.xlsx",
+                             col_types = c(rep("guess",4),"date",rep("guess",6)))
+    print("Selecting relevant sample")
+    AVENIO_sele <- AVENIO_runs[AVENIO_runs$CPR == CPR,]
+    AVENIO_sele <- AVENIO_sele[!is.na(AVENIO_sele$CPR),] %>% 
+        dplyr::select(Sample_name, Sample_note, Material, Notes)
+    print("Marging run and variant information")
+    df <- df %>% 
+        dplyr::left_join(AVENIO_sele, by = c("Sample.ID" = "Sample_name")) %>% 
+        dplyr::select(sample_index,
+               Mutation.Class,Gene,Amino.Acid.Change,
+               Variant.Description,Flags,Allele.Fraction,
+               Variant.Depth,Unique.Depth,Analysis.Name,Sample.ID,
+               Sample_note, Material, Notes) %>%
+        dplyr::mutate(Variant.Description = gsub(" variant","",Variant.Description)) %>% 
+        dplyr::tibble()
+    colnames(df) <- c("sample_index","Class","Gene","AA","Description",
+                      "Flags","MAF","Variant_depth","Unique_depth","Analysis",
+                      "Sample.ID","Sample_note","Material","Notes")
+    if(synonymous){
+        df <- df
+    }
+    else{
+        print("Removing Synonymous variants")
+        df <- df %>% 
+            dplyr::filter(!grepl("Synonymous",Description))
+    }
+    return(list("Patient CPR:" = CPR,"Results" = df))
+}
