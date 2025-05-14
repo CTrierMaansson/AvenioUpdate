73f8e84c2acd9dd6c9f34023190926b099c61902
Christoffer Trier Månsson <ctm@clin.au.dk>
2025-05-14 12:03:33 +0200

    1.6.5 Fixing issue with EML4-ALK variant classification if EML4 breakpoint cannot be estimated

--- R/add_DNAfusion_res.R
+++ R/add_DNAfusion_res.R
@@ -33,7 +33,6 @@ add_DNAfusion_res <- function(df,
         bam_file <- paste0(repo_path,"/",sampleID,"-",panelID,"/PosDeduped-",sampleID,".bam")
         DNAfusion_res <- DNAfusion::EML4_ALK_analysis(bam_file)
         if(length(DNAfusion_res) != 0){
-            message(paste0("EML4-ALK fusion detected with DNAfusion for sample: ", sample_ind))
             clipped_reads <- max(c(as.numeric(max(DNAfusion_res$breakpoint_ALK)),
                                    as.numeric(max(DNAfusion_res$breakpoint_EML4))),
                                  na.rm = T)
@@ -65,23 +64,31 @@ add_DNAfusion_res <- function(df,
                 MAF <- NA
                 ALK_depth <- NA
             }
-            message(paste0("Establishing the EML4-ALK variant"))
-            fusion_variant_res <- DNAfusion::find_variants(bam_file)
-            if(is.list(fusion_variant_res)){
-                if(is.data.frame(fusion_variant_res)){
-                    fusion_variant <- paste0(fusion_variant_res$Variant)
-                    exon <- paste0("ALK_i",fusion_variant_res$Intron_ALK,
-                                   ";EML4_i",fusion_variant_res$Intron_EML4)
-                }else{
-                    fusion_variant <- "Not classified"
-                    exon <- paste0("ALK_i",fusion_variant_res$ALK_intron,
-                                   ";EML4_i",fusion_variant_res$EML4_intron)
+            if(logic_EML4_breakpoint){
+                message(paste0("Establishing the EML4-ALK variant"))
+                fusion_variant_res <- DNAfusion::find_variants(bam_file)
+                if(is.list(fusion_variant_res)){
+                    if(is.data.frame(fusion_variant_res)){
+                        fusion_variant <- paste0(fusion_variant_res$Variant)
+                        exon <- paste0("ALK_i",fusion_variant_res$Intron_ALK,
+                                       ";EML4_i",fusion_variant_res$Intron_EML4)
+                    }else{
+                        fusion_variant <- "Not classified"
+                        exon <- paste0("ALK_i",fusion_variant_res$ALK_intron,
+                                       ";EML4_i",fusion_variant_res$EML4_intron)
+                    }
+                } 
+                else{
+                    fusion_variant <- fusion_variant_res
+                    exon <- paste0("ALK_i",NA,
+                                   ";EML4_i",NA)
                 }
-            } 
-            else{
-                fusion_variant <- fusion_variant_res
+            }else{
+                message(paste0("Could not establish EML4 breakpoint and \n",
+                               "thefore cannot establish EML4-ALK variant"))
                 exon <- paste0("ALK_i",NA,
                                ";EML4_i",NA)
+                fusion_variant <- "Not classified"
             }
             res_df <- data.frame(sample_index = sample_ind,
                                  Sample.ID = sampleID,
@@ -120,4 +127,35 @@ add_DNAfusion_res <- function(df,
     df <- df %>% 
         dplyr::arrange(sample_index,Gene)
     return(df)
+}
+add_DNAfusion_res(df = samples,
+                  repo_path = Directory,
+                  sample_info = AVENIO_runs_select)
+clipped_reads <- max(c(as.numeric(max(res$breakpoint_ALK)),
+                       as.numeric(max(res$breakpoint_EML4))),
+                     na.rm = T)
+if(is.character(names(which.max(res$breakpoint_ALK)))){
+    logic_ALK_breakpoint = T
+    fusion_position_1 <- paste0("chr2:",
+                                names(which.max(res$breakpoint_ALK)))
+}else{
+    fusion_position_1 <- paste0("chr2:","NA")
+    logic_ALK_breakpoint = F
+}
+if(is.character(names(which.max(res$breakpoint_EML4)))){
+    logic_EML4_breakpoint = T
+    fusion_position_2 <- paste0("chr2:",
+                                names(which.max(res$breakpoint_EML4)))
+}else{
+    fusion_position_2 <- paste0("chr2:","NA")
+    logic_EML4_breakpoint = F
+}
+fusion_position <- paste0(fusion_position_1,";",fusion_position_2)
+if(logic_ALK_breakpoint){
+    MAF_num <- round((clipped_reads/res$read_depth_ALK)*100,digits = 2)
+    MAF <- paste0(MAF_num,"%")
+    ALK_depth <- as.character(res$read_depth_ALK)
+}else{
+    MAF <- NA
+    ALK_depth <- NA
 }
