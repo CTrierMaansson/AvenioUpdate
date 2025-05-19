54c01404cedab6c6baf64d3aef1b9b5b65bad9a9
Christoffer Trier Månsson <ctm@clin.au.dk>
2025-05-19 11:44:28 +0200

    1.9.0 Adding zzz.R for package version control

---
+++ R/zzz.R
@@ -0,0 +1,11 @@
+.onAttach <- function(libname, pkgname) {
+    current_version <- packageVersion(pkgname)
+    required_version <- "1.8"
+    
+    if (current_version <= package_version(required_version)) {
+        packageStartupMessage(
+            paste0("WARNING: ", pkgname, " version ", current_version, 
+                   " is outdated. Please update to a version > ", required_version, ".")
+        )
+    }
+}
